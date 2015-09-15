d3e433bc411b490ac3ed33e8c2c67980f8847712
pjfl <github@roxsoft.co.uk>
2015-09-15 02:41:08 +0100

    Releasing v0.12

--- lib/Dist/Zilla/Plugin/ManifestInRoot.pm
+++ lib/Dist/Zilla/Plugin/ManifestInRoot.pm
@@ -1,7 +1,7 @@
 package Dist::Zilla::Plugin::ManifestInRoot;
 
 use namespace::autoclean;
-use version; our $VERSION = qv( sprintf '0.11.%d', q$Rev: 4 $ =~ /\d+/gmx );
+use version; our $VERSION = qv( sprintf '0.12.%d', q$Rev: 1 $ =~ /\d+/gmx );
 
 use Moose;
 use Dist::Zilla::File::FromCode;
@@ -91,7 +91,7 @@ Dist::Zilla::Plugin::ManifestInRoot - Puts the MANIFEST file in the project root
 
 =head1 Version
 
-This documents version v0.11.$Rev: 4 $ of L<Dist::Zilla::Plugin::ManifestInRoot>
+This documents version v0.12.$Rev: 1 $ of L<Dist::Zilla::Plugin::ManifestInRoot>
 
 =head1 Description
 
